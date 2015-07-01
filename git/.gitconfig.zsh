847f625855cfebed30cb42ed4e8a6ad9906038cd
Mike Kobit <mkobit@gmail.com>
2015-06-30 22:22:49 -0500

    Add some .git settings and keep track of some useful notes that might be used when writing install script

---
+++ git/.gitconfig.zsh
@@ -0,0 +1,21 @@
+[user]
+	email = mkobit@gmail.com
+	name = Mike Kobit
+[core]
+	editor = atom --wait
+	autocrlf = input
+[color]
+	ui = true
+[alias]
+	st = status
+	cm = commit -m
+	aa = add --all
+	ci = commit
+	lg = log --graph --pretty=format:'%C(yellow)%h%C(cyan)%d%Creset %s %C(white)- %an, %ar%Creset'
+	ll = log --stat --abbrev-commit
+	co = checkout
+	cob = checkout -b
+	sync = pull --rebase
+	br = branch
+[branch]
+	autosetuprebase = always
