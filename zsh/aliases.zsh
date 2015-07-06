37706cde2db8095ce0e31d17297388dd794e7c69
Mike Kobit <mkobit@gmail.com>
2015-07-05 22:04:44 -0500

    Add basic zshrc and aliases file with simple symbolic link install script

---
+++ zsh/aliases.zsh
@@ -0,0 +1,2 @@
+# Windows KeePass stored locally and ran with Mono
+alias keepass='mono ~/Programs/KeePass-2.29/KeePass.exe'
---
+++ zsh/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+# basic symlink both files here
+ln -sinr ./zshrc.zsh $HOME/.zshrc
+ln -sinr ./aliases.zsh $HOME/.aliases
