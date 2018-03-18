a6bb762a3c656dbad79aef901bd53b4eb1092e02
Mike Kobit <mkobit@gmail.com>
2018-03-18 15:29:30 -0500

    Add hack to deal with host key checking for GitLab.com

--- buildSrc/src/main/kotlin/dotfilesbuild/io/git/gitActions.kt
+++ buildSrc/src/main/kotlin/dotfilesbuild/io/git/gitActions.kt
@@ -1,9 +1,23 @@
 package dotfilesbuild.io.git
 
+import com.jcraft.jsch.Session
 import mu.KotlinLogging
 import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.transport.JschConfigSessionFactory
+import org.eclipse.jgit.transport.OpenSshConfig
+import org.eclipse.jgit.transport.SshTransport
 import java.io.File
 import javax.inject.Inject
+
+private val sessionConfigFactory = object : JschConfigSessionFactory() {
+  override fun configure(hc: OpenSshConfig.Host, session: Session) {
+    if (hc.hostName.contains("gitlab.com")) {
+      // TODO: Hack for host key checking for GitLab on laptop not doing what I expect
+      // https://stackoverflow.com/questions/13396534/unknownhostkey-exception-in-accessing-github-securely
+      session.setConfig("StrictHostKeyChecking", "no")
+    }
+  }
+}
 
 class CloneAction @Inject constructor(
     private val directory: File,
@@ -18,6 +32,11 @@ class CloneAction @Inject constructor(
         .setRemote("origin")
         .setURI(remoteUrl)
         .setDirectory(directory)
+        .setTransportConfigCallback { transport ->
+          if (transport is SshTransport) {
+            transport.sshSessionFactory = sessionConfigFactory
+          }
+        }
         .call().use {
       LOGGER.info { "Cloned $directory to $remoteUrl" }
     }
@@ -55,6 +74,11 @@ class PullAction @Inject constructor(
       it.pull()
           .setRebase(true)
           .setRemote(remote)
+          .setTransportConfigCallback { transport ->
+            if (transport is SshTransport) {
+              transport.sshSessionFactory = sessionConfigFactory
+            }
+          }
           .call()
       LOGGER.info { "Pulled origin for $repository" }
     }
