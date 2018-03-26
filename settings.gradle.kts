24fbd148b9a361312f3f03d6584ea88fd1770bc6
Mike Kobit <mkobit@gmail.com>
2018-03-26 17:35:23 -0500

    Switch settings file to Kotlin

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+rootProject.name = "dotfiles"
+rootProject.buildFileName = "build.gradle.kts"
+
+include("sidekick-service")
+
+rootProject.children.forEach { project ->
+  val replacedName = project.name.run {
+    val parts = split("-").toList()
+    val suffix = parts.drop(1).joinToString("") { "${it[0].toUpperCase()}${it.substring(1)}" }
+    "${parts[0]}$suffix"
+  }
+  project.buildFileName = "$replacedName.gradle.kts"
+}
