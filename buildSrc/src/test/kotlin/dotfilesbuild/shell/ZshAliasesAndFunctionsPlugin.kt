1c4a1490675b34ef4af202aa215aed9a530ec8aa
Mike Kobit <mkobit@gmail.com>
2018-11-15 10:39:08 -0600

    Add in placeholder plugin to make sure I don't forget about the `zsh/*` files that currently get sourced

---
+++ buildSrc/src/test/kotlin/dotfilesbuild/shell/ZshAliasesAndFunctionsPlugin.kt
@@ -0,0 +1,12 @@
+package dotfilesbuild.shell
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class ZshAliasesAndFunctionsPlugin : Plugin<Project> {
+  override fun apply(target: Project) {
+    target.run {
+      TODO()
+    }
+  }
+}
---
+++ buildSrc/src/test/kotlin/dotfilesbuild/shell/ZshAliasesAndFunctionsPluginTest.kt
@@ -0,0 +1,12 @@
+package dotfilesbuild.shell
+
+import org.junit.jupiter.api.Test
+import testsupport.jupiter.NotImplementedYet
+
+internal class ZshAliasesAndFunctionsPluginTest {
+  @NotImplementedYet
+  @Test
+  internal fun `source functions of source controlled files added to generated zsh file`() {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+}
