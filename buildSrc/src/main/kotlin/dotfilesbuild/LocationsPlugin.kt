c219fab2a88fd3c5a390853b4ab20a7522bd7548
Mike Kobit <mkobit@gmail.com>
2019-08-07 08:42:10 -0500

    Extend location extension to include directories for managed and unmanaged files

--- buildSrc/src/main/kotlin/dotfilesbuild/LocationsPlugin.kt
+++ buildSrc/src/main/kotlin/dotfilesbuild/LocationsPlugin.kt
@@ -3,11 +3,15 @@ package dotfilesbuild
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.file.Directory
+import org.gradle.api.file.ProjectLayout
 import org.gradle.api.model.ObjectFactory
 import org.gradle.kotlin.dsl.add
 import javax.inject.Inject
 
-class LocationsPlugin @Inject constructor(private val objectFactory: ObjectFactory) : Plugin<Project> {
+class LocationsPlugin @Inject constructor(
+  private val objectFactory: ObjectFactory,
+  private val projectLayout: ProjectLayout
+) : Plugin<Project> {
   override fun apply(target: Project) {
     target.run {
       val userHome: String = System.getProperty("user.home")
@@ -17,16 +21,18 @@ class LocationsPlugin @Inject constructor(private val objectFactory: ObjectFacto
       }
 
       val home = LocationsExtension(
-          homeDirectory,
-          homeDirectory.dir("Workspace"),
-          homeDirectory.dir("Programs"),
-          homeDirectory.dir("Downloads")
+        homeDirectory,
+        homeDirectory.dir("Workspace"),
+        homeDirectory.dir("Programs"),
+        homeDirectory.dir("Downloads"),
+        projectLayout.buildDirectory.dir("managed"),
+        projectLayout.projectDirectory.dir(".unmanaged")
       )
 
       extensions.add(
-          LocationsExtension::class,
-          "locations",
-          home
+        LocationsExtension::class,
+        "locations",
+        home
       )
     }
   }
