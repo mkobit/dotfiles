b9e2bad857a16505f6a2c03b2d3e0d89ecc97945
Mike Kobit <mkobit@gmail.com>
2020-09-22 10:53:55 -0500

    More depepdency upgrades and shrinkage

--- buildSrc/buildSrc.gradle.kts
+++ buildSrc/buildSrc.gradle.kts
@@ -22,6 +22,7 @@ ktlint {
 }
 
 kotlinDslPluginOptions {
+  jvmTarget.set("11")
   experimentalWarning.set(false)
 }
 
@@ -30,47 +31,30 @@ java {
   targetCompatibility = JavaVersion.VERSION_11
 }
 
-fun Configuration.useDotfilesDependencyRecommendations() {
-  val arrowKtVersion = "0.9.0"
-  val jacksonVersion = "2.9.9"
-  val junitJupiterVersion = "5.4.2"
-  val junitPlatformVersion = "1.4.2"
-  val kodeinDiVersion = "6.3.3"
-  val kotlinxCoroutinesVersion = "1.3.0"
-  val ktorVersion = "1.2.3"
-  val okHttpVersion = "4.0.1"
+fun Configuration.useBuildSrcDependencies() {
+  val arrowKtVersion = "0.10.0"
+  val junitJupiterVersion = "5.7.0"
+  val junitPlatformVersion = "1.7.0"
+  val okHttpVersion = "4.9.0"
   val log4jVersion = "2.12.0"
-  val minutestVersion = "1.7.0"
-  val retrofitVersion = "2.5.0"
+  val minutestVersion = "1.11.0"
   val slf4jVersion = "1.7.26"
-  val striktVersion = "0.21.1"
-  val testContainersVersion = "1.12.0"
+  val striktVersion = "0.26.1"
   resolutionStrategy.eachDependency {
     when (requested.group) {
       "com.squareup.okhttp3" -> useVersion(okHttpVersion)
-      "com.fasterxml.jackson.datatype" -> useVersion(jacksonVersion)
-      "com.fasterxml.jackson.core" -> useVersion(jacksonVersion)
-      "com.fasterxml.jackson.module" -> useVersion(jacksonVersion)
-      "com.squareup.retrofit2" -> useVersion(retrofitVersion)
       "dev.minutest" -> useVersion(minutestVersion)
       "io.arrow-kt" -> useVersion(arrowKtVersion)
-      "io.ktor" -> useVersion(ktorVersion)
       "io.strikt" -> useVersion(striktVersion)
       "org.apache.logging.log4j" -> useVersion(log4jVersion)
-      "org.jetbrains.kotlinx" -> when {
-        requested.name.startsWith("kotlinx-coroutines") && !requested.name.contains("io") ->
-          useVersion(kotlinxCoroutinesVersion)
-      }
-      "org.kodein.di" -> useVersion(kodeinDiVersion)
       "org.junit.jupiter" -> useVersion(junitJupiterVersion)
       "org.junit.platform" -> useVersion(junitPlatformVersion)
       "org.slf4j" -> useVersion(slf4jVersion)
-      "org.testcontainers" -> useVersion(testContainersVersion)
     }
   }
 }
 
-configurations.all { useDotfilesDependencyRecommendations() }
+configurations.all { useBuildSrcDependencies() }
 
 dependencies {
   // https://github.com/gradle/kotlin-dsl/issues/430
@@ -79,10 +63,9 @@ dependencies {
 
   implementation("io.github.microutils:kotlin-logging:1.12.0")
 
-  implementation("io.arrow-kt:arrow-core-data")
-  implementation("io.arrow-kt:arrow-core-extensions")
+  implementation("io.arrow-kt:arrow-core")
+  implementation("io.arrow-kt:arrow-syntax")
 
-  implementation("com.squareup.retrofit2:retrofit:2.5.0")
   implementation("com.squareup.okhttp3:okhttp")
   implementation("org.eclipse.jgit:org.eclipse.jgit:5.9.0.202009080501-r")
 
