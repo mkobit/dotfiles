611bc63bcaf9b257e8fa688054f59e6a7c5a93ac
Mike Kobit <mkobit@gmail.com>
2021-02-11 14:18:38 -0600

    Switch to different generation of Git config files

--- subprojects/shell/git/git.gradle.kts
+++ subprojects/shell/git/git.gradle.kts
@@ -1,8 +1,5 @@
 import dotfilesbuild.home
 import dotfilesbuild.projectFile
-import dotfilesbuild.io.file.EditFile
-import dotfilesbuild.io.file.Symlink
-import dotfilesbuild.io.file.content.SetContent
 
 plugins {
   dotfilesbuild.`dotfiles-lifecycle`
@@ -21,39 +18,6 @@ application {
 }
 
 tasks {
-  val gitConfigGeneration by registering(EditFile::class) {
-    val gitConfigGeneral = projectFile("gitconfig/gitconfig_general.dotfile")
-    val gitConfigPersonal = projectFile("gitconfig/gitconfig_personal.dotfile")
-    editActions.set(listOf(
-      SetContent {
-        """
-                [include]
-                    path = ${gitConfigGeneral.asFile.absolutePath}
-                [includeIf "gitdir:${project.rootDir.absolutePath}/"]
-                    path = ${gitConfigPersonal.asFile.absolutePath}
-                [includeIf "gitdir:${personalWorkspaceDirectory.asFile.absolutePath}/"]
-                    path = ${gitConfigPersonal.asFile.absolutePath}
-                [includeIf "gitdir:${codeLabWorkspaceDirectory.asFile.absolutePath}/"]
-                    path = ${gitConfigPersonal.asFile.absolutePath}
-                [includeIf "gitdir:${workWorkspaceDirectory.asFile.absolutePath}/"]
-                    path = ${home.file(".gitconfig_work").asFile.absolutePath}
-            """.trimIndent()
-      }
-    ))
-    file.set(layout.buildDirectory.file("generated/.gitconfig"))
-  }
-
-  val symlinkGeneratedGitFile  by registering(Symlink::class) {
-    dependsOn(gitConfigGeneration)
-    source.set(gitConfigGeneration.flatMap { it.output })
-    destination.set(home.file(".gitconfig"))
-  }
-
-  val symlinkGitIgnoreGlobal by registering(Symlink::class) {
-    source.set(projectFile("gitconfig/gitignore_global.dotfile"))
-    destination.set(home.file(".gitignore_global"))
-  }
-
   (run) {
     val outputDir = layout.buildDirectory.dir("generated-git")
     outputs.dir(outputDir)
@@ -70,7 +34,7 @@ tasks {
   }
 
   dotfiles {
-    dependsOn(symlinkGeneratedGitFile, symlinkGitIgnoreGlobal, run)
+    dependsOn(run)
   }
 }
 
