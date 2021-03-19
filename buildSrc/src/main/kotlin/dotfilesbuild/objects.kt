f610bdd8155fa2365ad4008d7f0d16b4cfe00f45
Mike Kobit <mkobit@gmail.com>
2021-03-19 11:02:26 -0500

    Add utility methods for creating provider with value

---
+++ buildSrc/src/main/kotlin/dotfilesbuild/objects.kt
@@ -0,0 +1,11 @@
+package dotfilesbuild
+
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.provider.Provider
+import org.gradle.kotlin.dsl.property
+
+inline fun <reified T> ObjectFactory.property(value: T?) =
+  property<T>().also { it.set(value) }
+
+inline fun <reified T> ObjectFactory.property(provider: Provider<out T>) =
+  property<T>().also { it.set(provider) }
