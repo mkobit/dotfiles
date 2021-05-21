82bb20524da5e06a2c3562fcf6be3fe4dd67ccc5
Mike Kobit <mkobit@gmail.com>
2021-05-21 09:24:30 -0500

    Start introducing vim plugin resolution

--- subprojects/shell/vim/build.gradle.kts
+++ subprojects/shell/vim/build.gradle.kts
@@ -2,7 +2,39 @@ plugins {
   id("dotfilesbuild.dotfiles-lifecycle")
 }
 
+val vimPluginsDir = layout.buildDirectory.dir("vim-plugin-downloads")
+
+repositories {
+  exclusiveContent {
+    forRepository {
+      ivy {
+        name = "github"
+        url = uri("https://github.com")
+        patternLayout {
+          artifact("/[organisation]/[module]/archive/[revision].[ext]")
+        }
+        metadataSources { artifact() }
+      }
+    }
+    filter {
+      includeGroup("junegunn")
+    }
+  }
+}
+
+val vimPlugins by configurations.creating
+
+dependencies {
+  vimPlugins("junegunn:vim-plug:master@zip")
+}
+
+
 tasks {
+  val resolveVimPlugins by registering(Sync::class) {
+    from(vimPlugins)
+    into(vimPluginsDir)
+  }
+
   val stageVimFiles by registering(Sync::class) {
     from(layout.projectDirectory.dir("config"))
     into(layout.buildDirectory.dir("generated-vim-config-staging"))
