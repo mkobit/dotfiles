90c6e7cc116d41a23fa42ad000e46e7b599286fd
Mike Kobit <mkobit@gmail.com>
2024-07-14 16:23:34 -0500

    More Gradle removals

--- testing/kotest-support/src/commonMain/kotlin/io/mkobit/testing/kotest/condition/enabledIf.kt
+++
@@ -1,8 +0,0 @@
-package io.mkobit.testing.kotest.condition
-
-import io.kotest.core.test.Enabled
-import io.kotest.core.test.EnabledOrReasonIf
-
-val NotImplemented: EnabledOrReasonIf = {
-  Enabled.disabled("not implemented yet")
-}
